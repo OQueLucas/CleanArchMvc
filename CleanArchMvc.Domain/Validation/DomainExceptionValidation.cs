f6a289828f485a1cbe3ee396f5f87d6f17a7b671
OQueLucas <l_queiroz@outlook.com.br>
2024-07-23 20:31:19 -0300

    Isolando modelos de domínio e validando campos

---
+++ CleanArchMvc.Domain/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,13 @@
+﻿namespace CleanArchMvc.Domain.Validation
+{
+    public class DomainExceptionValidation : Exception
+    {
+        public DomainExceptionValidation(string error) : base(error) { }
+
+        public static void When(bool hasErrro, string error)
+        {
+            if (hasErrro) 
+                throw new DomainExceptionValidation(error);
+        }
+    }
+}
