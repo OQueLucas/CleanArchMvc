2267a2763007141184b9d173c3a37d5f8fef08b5
OQueLucas <l_queiroz@outlook.com.br>
2024-07-24 23:55:58 -0300

    Adicionado testes de unidades para as Domain Entitities

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -13,7 +13,7 @@ namespace CleanArchMvc.Domain.Entities
 
         public Category(int id, string name)
         {
-            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
             Id = id;
             ValidateDomain(name);
         }
@@ -27,7 +27,7 @@ namespace CleanArchMvc.Domain.Entities
 
         private void ValidateDomain(string name)
         {
-            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.Name is required");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
             DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
 
             Name = name;
