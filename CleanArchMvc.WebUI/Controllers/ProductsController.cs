aea96f7deb2b0ece81b20fcb8abcb1fea608a406
OQueLucas <l_queiroz@outlook.com.br>
2024-07-31 23:49:32 -0300

    Adicionando Product Details View

--- CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -9,11 +9,13 @@ namespace CleanArchMvc.WebUI.Controllers
     {
         private readonly IProductService _productService;
         private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _environment;
 
-        public ProductsController(IProductService productService, ICategoryService categoryService)
+        public ProductsController(IProductService productService, ICategoryService categoryService, IWebHostEnvironment environment)
         {
             _productService = productService;
             _categoryService = categoryService;
+            _environment = environment;
         }
 
         [HttpGet]
@@ -71,6 +73,11 @@ namespace CleanArchMvc.WebUI.Controllers
                 await _productService.Update(productDTO);
                 return RedirectToAction(nameof(Index));
             }
+            else
+            {
+                ViewBag.CategoryId =
+                            new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+            }
             return View(productDTO);
         }
 
@@ -91,6 +98,24 @@ namespace CleanArchMvc.WebUI.Controllers
         {
             await _productService.Delete(id);
             return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var productDto = await _productService.GetById(id);
+
+            if (productDto == null) return NotFound();
+
+            var wwwroot = _environment.WebRootPath;
+
+            var image = Path.Combine(wwwroot, "images\\" + productDto.Image);
+            var exists = System.IO.File.Exists(image);
+            ViewBag.ImageExist = exists;
+
+            return View(productDto);
         }
     }
 }
